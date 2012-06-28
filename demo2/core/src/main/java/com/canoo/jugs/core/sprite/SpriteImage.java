d7168e0d1ea6e7b5c89ed516e54489b104d9d9bd
Erik Jan de Wit <erikjan.dewit@gmail.com>
2012-06-28 08:58:46 +0200

    formatted

--- demo2/core/src/main/java/com/canoo/jugs/core/sprite/SpriteImage.java
+++ demo2/core/src/main/java/com/canoo/jugs/core/sprite/SpriteImage.java
@@ -21,37 +21,37 @@ import playn.core.Image;
  * Represents the data associated with a single sprite.
  */
 class SpriteImage {
-  private final Image image;
-  private final int x;
-  private final int y;
-  private final int width;
-  private final int height;
+   private final Image image;
+   private final int x;
+   private final int y;
+   private final int width;
+   private final int height;
 
-  public SpriteImage(final Image image, int x, int y, int width, int height) {
-    this.image = image;
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-  }
+   public SpriteImage(final Image image, int x, int y, int width, int height) {
+      this.image = image;
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+   }
 
-  public Image image() {
-    return image;
-  }
+   public Image image() {
+      return image;
+   }
 
-  public int height() {
-    return height;
-  }
+   public int height() {
+      return height;
+   }
 
-  public int width() {
-    return width;
-  }
+   public int width() {
+      return width;
+   }
 
-  public int x() {
-    return x;
-  }
+   public int x() {
+      return x;
+   }
 
-  public int y() {
-    return y;
-  }
+   public int y() {
+      return y;
+   }
 }
--- demo2/core/src/main/java/com/canoo/jugs/core/sprite/SpriteLoader.java
+++ demo2/core/src/main/java/com/canoo/jugs/core/sprite/SpriteLoader.java
@@ -28,164 +28,164 @@ import static playn.core.PlayN.*;
 // TODO(pdr): the two getSprite() methods are messy, clean them up.
 public class SpriteLoader {
 
-    // prevent instantiation
-    private SpriteLoader() {
-    }
+   // prevent instantiation
+   private SpriteLoader() {
+   }
 
-    /**
-     * Return a {@link Sprite}, given a path to the image and a path to the json sprite description.
-     * <p/>
-     * json data should be in the following format:
-     * <p/>
-     * <pre>
-     * {
-     *   "sprites": [
-     *     {"id": "sprite_0", "x": 30, "y": 30, "w": 37, "h": 37},
-     *     {"id": "sprite_1", "x": 67, "y": 30, "w": 37, "h": 37},
-     *     {"id": "sprite_2", "x": 104, "y": 30, "w": 37, "h": 37},
-     *     {"id": "sprite_3", "x": 141, "y": 30, "w": 37, "h": 37}
-     * ]}
-     * </pre>
-     */
-    public static Sprite getSprite(String imagePath, String jsonPath) {
-        Image image = assets().getImage(imagePath);
-        final Image[] images = new Image[]{image};
-        // temp image to prevent NPE if using the Sprite's Layer (Sprite.getLayer()) before the image
-        // has loaded or before a sprite has been set (Sprite.setSprite()).
-        final Image tempImage = graphics().createImage(1, 1);
-        final Sprite sprite = new Sprite(graphics().createImageLayer(tempImage));
+   /**
+    * Return a {@link Sprite}, given a path to the image and a path to the json sprite description.
+    * <p/>
+    * json data should be in the following format:
+    * <p/>
+    * <pre>
+    * {
+    *   "sprites": [
+    *     {"id": "sprite_0", "x": 30, "y": 30, "w": 37, "h": 37},
+    *     {"id": "sprite_1", "x": 67, "y": 30, "w": 37, "h": 37},
+    *     {"id": "sprite_2", "x": 104, "y": 30, "w": 37, "h": 37},
+    *     {"id": "sprite_3", "x": 141, "y": 30, "w": 37, "h": 37}
+    * ]}
+    * </pre>
+    */
+   public static Sprite getSprite(String imagePath, String jsonPath) {
+      Image image = assets().getImage(imagePath);
+      final Image[] images = new Image[]{image};
+      // temp image to prevent NPE if using the Sprite's Layer (Sprite.getLayer()) before the image
+      // has loaded or before a sprite has been set (Sprite.setSprite()).
+      final Image tempImage = graphics().createImage(1, 1);
+      final Sprite sprite = new Sprite(graphics().createImageLayer(tempImage));
 
-        // load and parse json
-        assets().getText(jsonPath, new ResourceCallback<String>() {
-            @Override
-            public void done(String json) {
-                try {
-                    parseJson(images, sprite, json);
-                } catch (Throwable err) {
-                    sprite.error(err);
-                    return;
-                }
-                sprite.doneLoadingData();
+      // load and parse json
+      assets().getText(jsonPath, new ResourceCallback<String>() {
+         @Override
+         public void done(String json) {
+            try {
+               parseJson(images, sprite, json);
+            } catch (Throwable err) {
+               sprite.error(err);
+               return;
             }
+            sprite.doneLoadingData();
+         }
 
-            @Override
-            public void error(Throwable err) {
-                sprite.error(err);
+         @Override
+         public void error(Throwable err) {
+            sprite.error(err);
+         }
+      });
+
+      // set callback for image
+      image.addCallback(new ResourceCallback<Image>() {
+         @Override
+         public void done(Image resource) {
+            sprite.doneLoadingImages();
+         }
+
+         @Override
+         public void error(Throwable err) {
+            sprite.error(err);
+         }
+      });
+
+      return sprite;
+   }
+
+   /**
+    * Return a {@link Sprite}, given a path to the json sprite description.
+    * <p/>
+    * json data should be in the following format:
+    * <p/>
+    * <pre>
+    * {
+    *   "urls": ["images/peasprites2.png", "images/peasprites3.png"],
+    *   "sprites": [
+    *     {"id": "sprite_0", "url": 0, "x": 30, "y": 30, "w": 37, "h": 37},
+    *     {"id": "sprite_1", "url": 0, "x": 67, "y": 30, "w": 37, "h": 37},
+    *     {"id": "sprite_2", "url": 1, "x": 104, "y": 30, "w": 37, "h": 37},
+    *     {"id": "sprite_3", "url": 1, "x": 141, "y": 30, "w": 37, "h": 37}
+    * ]}
+    * </pre>
+    */
+   public static Sprite getSprite(String jsonPath) {
+      // temp image to prevent NPE if using the Sprite's Layer (Sprite.getLayer()) before the image
+      // has loaded or before a sprite has been set (Sprite.setSprite()).
+      final Image tempImage = graphics().createImage(1, 1);
+      final Sprite sprite = new Sprite(graphics().createImageLayer(tempImage));
+
+      // create asset watcher for the image assets
+      final AssetWatcher watcher = new AssetWatcher(new AssetWatcher.Listener() {
+         @Override
+         public void done() {
+            sprite.doneLoadingImages();
+         }
+
+         @Override
+         public void error(Throwable e) {
+            sprite.error(e);
+         }
+      });
+
+      // load and parse json, then add each image parsed from the json to the asset watcher to load
+      assets().getText(jsonPath, new ResourceCallback<String>() {
+         @Override
+         public void done(String json) {
+            try {
+               parseJson(null, sprite, json);
+               for (SpriteImage spriteImage : sprite.spriteImages()) {
+                  watcher.add(spriteImage.image());
+               }
+               watcher.start();
+            } catch (Throwable err) {
+               sprite.error(err);
+               return;
             }
-        });
+            sprite.doneLoadingData();
+         }
 
-        // set callback for image
-        image.addCallback(new ResourceCallback<Image>() {
-            @Override
-            public void done(Image resource) {
-                sprite.doneLoadingImages();
-            }
+         @Override
+         public void error(Throwable err) {
+            sprite.error(err);
+         }
+      });
 
-            @Override
-            public void error(Throwable err) {
-                sprite.error(err);
-            }
-        });
+      return sprite;
+   }
 
-        return sprite;
-    }
+   /**
+    * Parse a json sprite sheet and add the sprite images to the sheet.
+    * <p/>
+    * If images is null, the images urls are parsed from the json.
+    *
+    * @param images Image to associate with each {@link SpriteImage}, or null to parse from the json
+    * @param sprite Sprite to store the {@link SpriteImage}s
+    * @param json   json to parse
+    */
+   private static void parseJson(Image[] images, Sprite sprite, String json) {
+      Json.Object document = json().parse(json);
 
-    /**
-     * Return a {@link Sprite}, given a path to the json sprite description.
-     * <p/>
-     * json data should be in the following format:
-     * <p/>
-     * <pre>
-     * {
-     *   "urls": ["images/peasprites2.png", "images/peasprites3.png"],
-     *   "sprites": [
-     *     {"id": "sprite_0", "url": 0, "x": 30, "y": 30, "w": 37, "h": 37},
-     *     {"id": "sprite_1", "url": 0, "x": 67, "y": 30, "w": 37, "h": 37},
-     *     {"id": "sprite_2", "url": 1, "x": 104, "y": 30, "w": 37, "h": 37},
-     *     {"id": "sprite_3", "url": 1, "x": 141, "y": 30, "w": 37, "h": 37}
-     * ]}
-     * </pre>
-     */
-    public static Sprite getSprite(String jsonPath) {
-        // temp image to prevent NPE if using the Sprite's Layer (Sprite.getLayer()) before the image
-        // has loaded or before a sprite has been set (Sprite.setSprite()).
-        final Image tempImage = graphics().createImage(1, 1);
-        final Sprite sprite = new Sprite(graphics().createImageLayer(tempImage));
+      // parse image urls, if necessary
+      if (images == null || images.length == 0) {
+         Json.Array urls = document.getArray("urls");
+         Asserts.checkNotNull(urls, "No urls provided for sprite images");
+         images = new Image[urls.length()];
+         for (int i = 0; i < urls.length(); i++) {
+            images[i] = assets().getImage(urls.getString(i));
+         }
+      }
 
-        // create asset watcher for the image assets
-        final AssetWatcher watcher = new AssetWatcher(new AssetWatcher.Listener() {
-            @Override
-            public void done() {
-                sprite.doneLoadingImages();
-            }
-
-            @Override
-            public void error(Throwable e) {
-                sprite.error(e);
-            }
-        });
-
-        // load and parse json, then add each image parsed from the json to the asset watcher to load
-        assets().getText(jsonPath, new ResourceCallback<String>() {
-            @Override
-            public void done(String json) {
-                try {
-                    parseJson(null, sprite, json);
-                    for (SpriteImage spriteImage : sprite.spriteImages()) {
-                        watcher.add(spriteImage.image());
-                    }
-                    watcher.start();
-                } catch (Throwable err) {
-                    sprite.error(err);
-                    return;
-                }
-                sprite.doneLoadingData();
-            }
-
-            @Override
-            public void error(Throwable err) {
-                sprite.error(err);
-            }
-        });
-
-        return sprite;
-    }
-
-    /**
-     * Parse a json sprite sheet and add the sprite images to the sheet.
-     * <p/>
-     * If images is null, the images urls are parsed from the json.
-     *
-     * @param images Image to associate with each {@link SpriteImage}, or null to parse from the json
-     * @param sprite Sprite to store the {@link SpriteImage}s
-     * @param json   json to parse
-     */
-    private static void parseJson(Image[] images, Sprite sprite, String json) {
-        Json.Object document = json().parse(json);
-
-        // parse image urls, if necessary
-        if (images == null || images.length == 0) {
-            Json.Array urls = document.getArray("urls");
-            Asserts.checkNotNull(urls, "No urls provided for sprite images");
-            images = new Image[urls.length()];
-            for (int i = 0; i < urls.length(); i++) {
-                images[i] = assets().getImage(urls.getString(i));
-            }
-        }
-
-        // parse the sprite images
-        Json.Array spriteImages = document.getArray("sprites");
-        for (int i = 0; i < spriteImages.length(); i++) {
-            Json.Object jsonSpriteImage = spriteImages.getObject(i);
-            String id = jsonSpriteImage.getString("id");
-            int imageId = jsonSpriteImage.getInt("url"); // will return 0 if not specified
-            Asserts.checkElementIndex(imageId, images.length, "URL must be an index into the URLs array");
-            int x = jsonSpriteImage.getInt("x");
-            int y = jsonSpriteImage.getInt("y");
-            int width = jsonSpriteImage.getInt("w");
-            int height = jsonSpriteImage.getInt("h");
-            SpriteImage spriteImage = new SpriteImage(images[imageId], x, y, width, height);
-            sprite.addSpriteImage(id, spriteImage);
-        }
-    }
+      // parse the sprite images
+      Json.Array spriteImages = document.getArray("sprites");
+      for (int i = 0; i < spriteImages.length(); i++) {
+         Json.Object jsonSpriteImage = spriteImages.getObject(i);
+         String id = jsonSpriteImage.getString("id");
+         int imageId = jsonSpriteImage.getInt("url"); // will return 0 if not specified
+         Asserts.checkElementIndex(imageId, images.length, "URL must be an index into the URLs array");
+         int x = jsonSpriteImage.getInt("x");
+         int y = jsonSpriteImage.getInt("y");
+         int width = jsonSpriteImage.getInt("w");
+         int height = jsonSpriteImage.getInt("h");
+         SpriteImage spriteImage = new SpriteImage(images[imageId], x, y, width, height);
+         sprite.addSpriteImage(id, spriteImage);
+      }
+   }
 }
